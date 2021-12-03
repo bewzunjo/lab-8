57ced7dc784bf727b58ac48628b060d1517c1e32
bewzunjo <saran_kim@cmu.ac.th>
2021-12-03 14:00:51 +0700

    lab 8.1

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+    int a  =  20; double b = 30.0;
+    int c = 4;
+    double x = a - b/c;
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+    cout << "output: " << x << "\n";
     return 0;
 }
